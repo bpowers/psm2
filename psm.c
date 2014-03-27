0db8689322aac60fbd90ddf0c6cbd61db6baa373
Bobby Powers <bobbypowers@gmail.com>
2014-03-27 00:25:59 -0400

    work on reading from /proc

--- psm.c
+++ psm.c
@@ -27,13 +27,29 @@
 
 #include "config.h"
 
+#define COMM_MAX 16
+
 int n_cpu;
 bool show_heap;
 char *filter;
 char *argv0;
 
+typedef struct {
+	int npids;
+	char *name;
+	float pss;
+	float shared;
+	float heap;
+	float swapped;
+} MemInfo;
 
 static void die(const char *, ...);
+static MemInfo *meminfo(int);
+static void free_meminfo(MemInfo *);
+static int proc_name(MemInfo *, int);
+static int proc_mem(MemInfo *, int);
+static int proc_cmdline(int, char *buf, size_t len);
+static char *_readlink(char *);
 static void usage(void);
 static int *list_pids(void);
 
@@ -47,6 +63,111 @@ die(const char *fmt, ...)
 	va_end(args);
 
 	exit(EXIT_FAILURE);
+}
+
+char *
+_readlink(char *path)
+{
+	for (int len=64;; len*=2) {
+		char *b = malloc(len);
+		ssize_t n = readlink(path, b, len-1);
+		if (n == -1) {
+			free(b);
+			return NULL;
+		}
+		if (n < len) {
+			b[n] = '\0';
+			return b;
+		}
+		free(b);
+	}
+}
+
+MemInfo *
+meminfo(int pid)
+{
+	int err;
+	MemInfo *mi = calloc(sizeof(MemInfo), 1);
+	err = proc_name(mi, pid);
+	if (err) goto error;
+
+	err = proc_mem(mi, pid);
+	if (err) goto error;
+
+	return mi;
+error:
+	free_meminfo(mi);
+	return NULL;
+}
+
+void
+free_meminfo(MemInfo *mi)
+{
+	if (mi && mi->name)
+		free(mi->name);
+	free(mi);
+}
+
+int
+proc_name(MemInfo *mi, int pid)
+{
+	int n;
+	char path[32], buf[BUFSIZ];
+	snprintf(path, sizeof(path), "/proc/%d/exe", pid);
+
+	char *p = _readlink(path);
+	// we can't read the exe in 2 cases: the process has exited,
+	// or it refers to a kernel thread
+	if (!p)
+		return 0;
+
+	n = proc_cmdline(pid, buf, BUFSIZ);
+	if (n <= 0) {
+		free(p);
+		return -1;
+	}
+
+	if (strlen(buf) > COMM_MAX)
+		buf[COMM_MAX] = '\0';
+	if (strcmp(p, buf) >= 0)
+		mi->name = NULL;//strdup(basename(p));
+	else
+		mi->name = strdup(buf);
+	free(p);
+	return 0;
+}
+
+int
+proc_cmdline(int pid, char *buf, size_t len)
+{
+	int fd;
+	char path[32];
+	ssize_t n;
+
+	snprintf(path, sizeof(path), "/proc/%d/cmdline", pid);
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return -1;
+	n = read(fd, buf, len - 1);
+	if (n < 0)
+		die("proc_cmdline(%d): read failed: %s", pid, strerror(errno));
+	if (!n)
+		goto error;
+
+	buf[n] = '\0';
+
+	close(fd);
+	return n;
+error:
+	close(fd);
+	return -1;
+}
+
+int
+proc_mem(MemInfo *mi, int pid)
+{
+	return 0;
 }
 
 int *
@@ -98,7 +219,7 @@ usage(void)
 int
 main(int argc, char *const argv[])
 {
-	int err, procfd;
+	int err;
 	cpu_set_t n_cpu_set;
 	int *pids;
 
@@ -131,14 +252,11 @@ main(int argc, char *const argv[])
 	if (!pids)
 		die("list_pids failed\n");
 
-	procfd = open("/proc", O_DIRECTORY | O_CLOEXEC);
-	if (!procfd)
-		die("couldn't open /proc\n");
-
 	for (int *pid = pids; *pid; pid++) {
+		MemInfo *mi = meminfo(*pid);
+		free_meminfo(mi);
 	}
-
-	close(procfd);
+	free(pids);
 
 	return 0;
 }
