9f41f0a2b43feda0036f83ab1c8a40ae01e094ba
Bobby Powers <bobbypowers@gmail.com>
2016-01-18 17:02:57 -0500

    rusty get_pid

--- psm-rust/src/main.rs
+++ psm-rust/src/main.rs
@@ -2,12 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#![feature(convert)]
+#![feature(convert,str_char)]
 
 //extern crate libc;
 //use libc::geteuid;
 
 use std::fs;
+use std::io::{Error, ErrorKind};
 
 const PROC_PATH: &'static str = "/proc";
 
@@ -23,12 +24,34 @@ fn is_digit(d: char) -> bool {
     d >= '0' && d <= '9'
 }
 
-fn get_pids() -> Vec<i32> {
-    let mut pids: Vec<i32> = vec!();
+fn not_dir<T>() -> Result<T, Error> {
+    Err(Error::new(ErrorKind::NotFound, "not a directory"))
+}
 
-    match fs::read_dir(PROC_PATH) {
+fn get_pids<'a>() -> Result<Vec<i32>, String> {
+    let mut dir = fs::read_dir(PROC_PATH);
+    if dir.is_err() {
+	return Err(format!("read_dir({}): {}", PROC_PATH, dir.err().unwrap()));
+    }
+
+    // TODO: look at filter_map
+    let pids: Vec<i32> = dir.unwrap()
+	.filter(|e| e.is_ok()) // ignore bad dirent results
+	.map(|e| e.unwrap())   // make this a list of dirents
+	.filter(|e| is_digit(e.file_name().to_str().unwrap().char_at(0)))
+	.filter(|e| fs::metadata(e.path()).and_then(|md| if md.is_dir() { Ok("") } else { not_dir() }).is_ok())
+	.map(|e| i32::from_str_radix(e.file_name().to_str().unwrap(), 10).unwrap())
+	.collect();
+
+    Ok(pids)
+}
+/*
+    {
 	Err(err) => println!("read_dir({}): {}", PROC_PATH, err),
-	Ok(result) => for entry in result {
+	Ok(result) => {
+	    // result.filter()
+	}
+	    for entry in result {
 	    match entry {
 		Err(_) => (), // ignore
 		Ok(dent) => {
@@ -50,6 +73,7 @@ fn get_pids() -> Vec<i32> {
 
     return pids;
 }
+*/
 
 fn cmdinfos_for(pids: Vec<i32>) -> Vec<CmdInfo> {
     let mut infos: Vec<CmdInfo> = Vec::with_capacity(pids.len());
@@ -73,8 +97,13 @@ fn main() {
     // 	}
     // }
 
-    let pids = get_pids();
+    let pids_r = get_pids();
+    if pids_r.is_err() {
+	println!("get_pids: {}", pids_r.err().unwrap());
+	return;
+    }
 
+    let pids = pids_r.unwrap();
     // get details of all pids (possibly in parallel)
     let infos = cmdinfos_for(pids);
 
